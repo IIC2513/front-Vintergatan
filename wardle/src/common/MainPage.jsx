d5955cafae077ab79b239aaf1e2c216c844d2534
palettblad <adilsaberg@gmail.com>
2024-11-08 15:39:15 -0300

    css agregado para mainpage y reglas

--- wardle/src/common/MainPage.jsx
+++ wardle/src/common/MainPage.jsx
@@ -1,9 +1,30 @@
+import "./MainPage.css";
+
 export default function MainPage() {
-    return (
-        <>
-        <h1>Wardle!</h1>
-        <a href="/register">Registrar Usuario</a>
-        <a href="/rules">Reglas del Juego</a>
-        </>
-    )
+  return (
+    <>
+      <h1>WARdl!</h1>
+      <div className="Userinput">
+        <h2 className="title">Inicia sesión en tu cuenta</h2>
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="NOMBRE DE USUARIO..."
+          />
+          <input type="password" name="password" placeholder="CONTRASEÑA..." />
+          <button type="submit">➔</button>
+        </form>
+        <h2 className="title">
+          <a href="/register" className="register">o crea una nueva cuenta</a>
+        </h2>
+      </div>
+      <div className="Instructions">
+        <a href="/rules">REGLAS DEL JUEGO</a>
+        <a href="/about">CÓMO JUGAR</a> {/* todavía no existe la ruta */}
+        <a href="/visita">JUGAR COMO VISITA</a>{" "}
+        {/* todavía no existe la ruta */}
+      </div>
+    </>
+  );
 }
